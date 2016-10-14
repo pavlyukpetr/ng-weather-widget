495abd7f10222cb48da3623388fb3fbb528312cf
hartryfock <hartryfock@gmail.com>
2016-10-14 17:22:59 +0300

    change img folder

--- src/app/left-element/left-element.component.ts
+++ src/app/left-element/left-element.component.ts
@@ -12,18 +12,18 @@ export class LeftElementComponent implements OnInit {
   public places = [
     {
       phone: '+1 111 111 1111',
-      img1: 'images/1.jpg',
-      img2: 'images/b1.jpg'
+      img1: 'assets/images/1.jpg',
+      img2: 'assets/images/b1.jpg'
     },
     {
       phone: '+1 222 222 2222',
-      img1: 'images/2.jpg',
-      img2: 'images/b2.jpg'
+      img1: 'assets/images/2.jpg',
+      img2: 'assets/images/b2.jpg'
     },
     {
       phone: '+1 333 333 3333',
-      img1: 'images/3.jpg',
-      img2: 'images/b3.jpg'
+      img1: 'assets/images/3.jpg',
+      img2: 'assets/images/b3.jpg'
     }
   ]
 
